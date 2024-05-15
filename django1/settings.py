f5d725fecfa07238ac03611d57ca47b60e51c7c4
flaviovieiradev <analyst.flaviovieira@gmail.com>
2024-05-15 17:31:10 -0300

    Apontamento do urls.py do projeto para o urls.py da aplicação Core.

--- django1/settings.py
+++ django1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rj_3n_hyp5!3l)+ay7l*+8f8_9k-@1b)7732y*$tcxe8npui3^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'django1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
