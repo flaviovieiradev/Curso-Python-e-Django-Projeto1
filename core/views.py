31929716870996bc1d5f1cf2b2e66f5afc47510b
flaviovieiradev <analyst.flaviovieira@gmail.com>
2024-07-27 00:48:12 -0300

    Término da aula sobre o Django Shell.

--- core/views.py
+++ core/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 
 
 def index(request):
+    print(dir(request.user))
+    print(f'User: {request.user}')
+
+    if str(request.user) == 'AnonymousUser':
+        teste = 'Usuário não logado'
+    else:
+        teste = 'Usuário Logado'
     context = {
         'curso': 'Programação Web com Django Framework',
         'outro': 'Django é muito prático',
+        'logado': teste
     }
     return render(request,'index.html', context)
 
